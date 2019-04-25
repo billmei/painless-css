2ac60bcee788e0886b5c9ae10b803f5a367a861d
Bill Mei <billmei@users.noreply.github.com>
2019-04-25 16:23:08 -0400

    Add table of contents

--- main.js
+++ main.js
@@ -19,4 +19,24 @@ $(document).ready(function() {
     $('stylesheets').hide();
   });
 
+  $('.accordion-toggle').click(function() {
+    $(this).toggleClass('expanded').next('.accordion-content').slideToggle('fast');
+  });
+
+  $('.expand-all').click(function() {
+    $('.accordion-content').slideDown();
+    $(this).hide();
+    $('.collapse-all').show();
+    $('.accordion-toggle').removeClass('expanded');
+    $('.accordion-toggle').addClass('expanded');
+  })
+
+  $('.collapse-all').click(function() {
+    $('.accordion-content').slideUp();
+    $(this).hide();
+    $('.expand-all').show();
+    $('.accordion-toggle').addClass('expanded');
+    $('.accordion-toggle').removeClass('expanded');
+  })
+
 });
